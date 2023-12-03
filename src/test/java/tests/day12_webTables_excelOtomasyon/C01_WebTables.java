d2472b637fe6b570883f195603d20259b37a8c29
Gulnur <taskin.gulnur@gmail.com>
2023-12-04 00:03:57 +0100

    webtables

--- src/test/java/tests/day12_webTables_excelOtomasyon/C01_WebTables.java
+++ src/test/java/tests/day12_webTables_excelOtomasyon/C01_WebTables.java
@@ -9,63 +9,115 @@ import utilities.TestBase;
 import java.util.List;
 
 public class C01_WebTables extends TestBase {
+    // bu klasik yontemin uzerinde cok durmadim, ogrenmek icin cabalamdim
+    // cunku sonraki derste yani C02, C03 ve C04 class'inda bu isin daha pratik seklini yaptik
+    // bu class'i ahmet hoca'dan direk kopyala-yapistir yaptim
+    // burda bulunsun diye
 
     @Test
     public void test01(){
 
         //  1. “https://testotomasyonu.com/webtables2” sayfasina gidin
-
         driver.get("https://testotomasyonu.com/webtables2");
 
         //  2. Headers da bulunan basliklari yazdirin
+        List<WebElement> baslikElementleriList = driver.findElements(By.xpath("//div[@role='hdata']"));
 
-        List<WebElement> baslikWebelementList = driver.findElements(By.xpath("//div[@role='hdata']"));
-
-              //
-        List<String> baslikYazilariList = ReusableMethods.stringListeDonustur(baslikWebelementList);
-        System.out.println("Basliklar: "+ baslikYazilariList);
+        // bu liste webelementlerden olustugu icin hemen yazdiramayiz
+        List<String> baslikYazilariList = ReusableMethods.stringListeDonustur(baslikElementleriList);
+        System.out.println("Basliklar : " + baslikYazilariList);
 
         //  3. 3.sutunun basligini yazdirin
-
-        System.out.println("3.sutun basligi : "+baslikYazilariList.get(2));
+        System.out.println("3.sutun basligi : " + baslikYazilariList.get(2));
 
         //  4. Tablodaki tum datalari yazdirin
-
+        List<WebElement> tabloDataElementleriList = driver.findElements(By.xpath("//*[@role='tdata']"));
+        List<String> tabloDataYazilarList = ReusableMethods.stringListeDonustur(tabloDataElementleriList);
+        System.out.println("Tum tabloda bulunan datalar : " + tabloDataYazilarList);
 
         //  5. Tabloda kac tane cell (data) oldugunu yazdirin
-
-        System.out.println("tablodaki data sayisi :");
+        System.out.println("Tablodaki data sayisi : " + tabloDataElementleriList.size());
 
         //  6. Tablodaki satir sayisini yazdirin
-
+        List<WebElement> satirElementleriList = driver.findElements(By.xpath("//*[@role='trow']"));
+        System.out.println("Tablodaki urun satirlari sayisi : " + satirElementleriList.size());
 
         //  7. Tablodaki sutun sayisini yazdirin
-
-        System.out.println("tablodaki sutun sayisi: "+baslikYazilariList.size());
+        System.out.println("Tablodaki sutun sayisi : " + baslikElementleriList.size());
 
         //  8. Tablodaki 3.kolonu yazdirin
+        List<WebElement> ucuncuSutunElementleriList = driver.findElements(By.xpath("//*[@role='trow']/*[@role='tdata'][3]"));
+        List<String > ucuncuSutunYaziListesi = ReusableMethods.stringListeDonustur(ucuncuSutunElementleriList);
 
-        List<WebElement>ucuncuSutunElementleriList = driver.findElements(By.xpath("//*[@role='trow']/*[@role='tdata'][3]"));
-        List<String>ucuncuSutunYaziList = ReusableMethods.stringListeDonustur(ucuncuSutunElementleriList);
-        System.out.println("ucuncu sutun: "+ ucuncuSutunYaziList);
+        System.out.println("3. Sutun : " + ucuncuSutunYaziListesi);
 
         //  9. Tabloda " Category" si Furniture olan urunun fiyatini yazdirin
 
-           // herhangi bir satirin kategori bilgisi icin locate
-           // bir loop ile her bir satiri tek tek inceleyelim
-           // kategori degeri yani (2.data) furniture olan satirdaki
-           // fiyat degerini yani (3. data) yazdiralim
+        // bir loop ile her bir satiri tek tek inceleyelim
+        // kategori degeri (yani 2.data) Furniture olan satirdaki
+        // fiyat degerini(yani 3.data) yazdiralim
+        String satirdakiKategoriXpath ="";
+        String satirdakiFiyatXpath = "";
 
-        String satirdakiKategoriXpath="";
-        String satirdakiFiyatXpath="";
+        for (int i = 1; i <= satirElementleriList.size() ; i++) {
 
-        // 10. Bir method olusturun, Test sayfasindan satir ve sutun sayisini girdigimde bana datayi yazdirsin
+            // i. satirdaki kategori elementinin locate'i
+
+            satirdakiKategoriXpath = "((//*[@role='trow'])[" + i + "]/*[@role='tdata'])[2]";
+            satirdakiFiyatXpath = "((//*[@role='trow'])[" + i + "]/*[@role='tdata'])[3]";
+            if (driver.findElement(By.xpath(satirdakiKategoriXpath)).getText().equals("Furniture")){
+
+                System.out.println("Istenen urunun fiyati : " +
+                        driver.findElement(By.xpath(satirdakiFiyatXpath)).getText());
+            }
+
+        }
+
+        //10. Bir method olusturun, satir ve sutun sayisini girdigimde bana datayi yazdirsin
+
+        istedenHucredekiDatayiYazdir(2,3); // Istenen hucredeki data : $399.00
+        istedenHucredekiDatayiYazdir(1,2); // Istenen hucredeki data : Electronics
+
+        // yukardaki gorevi method'u kullanarak yapalim
+
+        // Tabloda " Category" si Furniture olan urunun fiyatini yazdirin
+
+        String satirdakiKategoriDegeri ="";
+        String satirdakiFiyatDegeri="";
+
+        for (int i = 1; i <=satirElementleriList.size() ; i++) {
+
+            satirdakiKategoriDegeri = istedenHucredekiDatayiDondur(i,2);
+            satirdakiFiyatDegeri = istedenHucredekiDatayiDondur(i,3);
+
+            if (satirdakiKategoriDegeri.equals("Furniture")){
+                System.out.println("Kategorisi Furniture olan urun fiyati : "+ satirdakiFiyatDegeri);
+            }
+        }
+
+        ReusableMethods.bekle(2);
+    }
+
+    public void istedenHucredekiDatayiYazdir(int satirNo, int sutunNo){
+
+        // ((//*[@role='trow'])[3]/*[@role='tdata'])[3]
+
+        String dinamikXpath = "((//*[@role='trow'])[" + satirNo + "]/*[@role='tdata'])[" + sutunNo + "]";
+
+        System.out.println("Istenen hucredeki data : " +
+                driver.findElement(By.xpath(dinamikXpath)).getText());
+    }
 
 
+    public String istedenHucredekiDatayiDondur(int satirNo, int sutunNo){
+
+        // ((//*[@role='trow'])[3]/*[@role='tdata'])[3]
+
+        String dinamikXpath = "((//*[@role='trow'])[" + satirNo + "]/*[@role='tdata'])[" + sutunNo + "]";
 
 
-
-
+        return driver.findElement(By.xpath(dinamikXpath)).getText();
     }
 
 }
+
--- src/test/java/tests/day12_webTables_excelOtomasyon/C02_ReadExcelOtomasyonu.java
+++ src/test/java/tests/day12_webTables_excelOtomasyon/C02_ReadExcelOtomasyonu.java
@@ -5,38 +5,66 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class C02 {
+public class C02_ReadExcelOtomasyonu {
 
     @Test
-    public void  excelOtomasyonTesti() throws IOException {
+    public void excelOtomasyonTesti() throws IOException {
 
+        // java ile bilgisayarimizdaki bir dosyaya ulasmak icin:
 
-
-        String dosyaYolu ="src\\test\\java\\tests\\day12_webTables_excelOtomasyon\\ulkeler.xlsx";
+        String dosyaYolu = "src/test/java/tests/day12_webTables_excelOtomasyon/ulkeler.xlsx";
 
         FileInputStream fileInputStream = new FileInputStream(dosyaYolu);
 
+        // java ile ulkeler.xlsx dosyasindaki bilgiler bir imput stream olarak alindi..
+
         /*
+            Bilgisayarimizdaki fiziki bir dosyayi
+            kodlarimiza direk eklememiz mumkun degil
+
+            Apache poi verdigimiz dosya yolundaki
+            fiziki excel dosyasinda bulunan tum datalari
+            fileInputStream objesi sayesinde okuyup,
+            o bilgileri kullanabilecegimiz
+            EXCEL dosyasinin KOPYASINI olusturur
+
+            biz de bu kopya excel uzerinde istedigimiz islemleri yapabiliriz
+
+            ONEMLI NOT :
+            bu test methodu icinde yaptigimiz tum islemler
+            excel uzerinde degil
+            45.satirin calistigi anda excel'de varolan bilgiler uzerindedir
+
+            satir ve sutun bilgileri index ile calisir
+            yani 0'dan baslar
 
          */
 
-        Workbook workbook = WorkbookFactory.create(fileInputStream);
+        Workbook workbook = WorkbookFactory.create(fileInputStream); // Workbook, apache poi'den geliyor
+        // burda soyle diyoruz Workbook'a: bana bir workbook olustur, WorkbookFactory'den,
+        // benim file imput stream bilgilerini de ona yukle...
+        // olusturdugumuz workbook'un icinde tum excel tablosu bilgileri var
 
-        Sheet sayfa1 = workbook.getSheet("sayfa1");
-        Row ucuncuSatir = sayfa1.getRow(3);
-        Cell ikinciData = ucuncuSatir.getCell(2);
+        Sheet sayfa1 = workbook.getSheet("Sayfa1"); // Sheet = sayfa
+        // parantez icine excel dosyasindaki sayfa ismi ne ise onu yaziyoruz tam olarak
 
-        System.out.println(ikinciData); //
+        Row ucuncuSatir = sayfa1.getRow(3); // Row = satir
+        // index=3, fiziki olarak 4.satira gider
+
+        Cell ikinciData = ucuncuSatir.getCell(2); // Cell = hucre
+        // index=2, fiziki olarak 3.dataya gider
+
+        System.out.println(ikinciData); // Cezayir
+
+        // 12.satirda bulunan ulkenin turkce isminin "Azerbaycan" oldugunu test edin
+
         String actualIstenenUlkeAdi = sayfa1.getRow(11).getCell(2).toString();
-        String expectedUlkeAdi ="Azerbaycan";
+        // burda sol taraf string oldugu icin,
+        // sag taraftaki excell bilgilerini toString yapmak zorundayiz
+        String expectedUlkeAdi = "Azerbaycan";
 
         Assert.assertEquals(expectedUlkeAdi,actualIstenenUlkeAdi);
-
-
-
-
     }
 }
